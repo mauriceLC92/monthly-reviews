fcc324893ad3c58fde45e1eae4558c9a658348c9
mauriceLC92 <mauricelecordier@gmail.com>
2022-10-18 20:36:57 +0200

    added tailwind and begun work on the markup

---
+++ config/questions.ts
@@ -0,0 +1,23 @@
+interface question {
+    title: string;
+    description: string;
+}
+
+export const reviewQuestions: question[] = [
+    {
+        title: "What were my biggest wins?",
+        description: "These are things that you will look back on with pride & fond memories at the end of the year."
+    },
+    {
+        title: "What were my biggest realizations?",
+        description: ""
+    },
+    {
+        title: "What am I going to do more of this month? Less of?",
+        description: "."
+    },
+    {
+        title: "What am I thinking about for the upcoming month?",
+        description: ""
+    },
+]
